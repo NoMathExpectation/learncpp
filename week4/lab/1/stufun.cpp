ecd1313665a503c0fc62645fa059298d1828551b
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-09-30 18:39:44 +0800

    fix lab4_1 sort bug

--- week4/lab/1/stufun.cpp
+++ week4/lab/1/stufun.cpp
@@ -3,43 +3,51 @@
 
 using namespace std;
 
-void inputstu(stuinfo stu[], int n) {
+void inputstu(stuinfo stu[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        cout << endl << "Please input student" << i + 1 << "'s name: ";
+        cout << endl
+             << "Please input student" << i + 1 << "'s name: ";
         cin >> stu[i].name;
-        cout << endl << "Please input student" << i + 1 << "'s three scores: ";
+        cout << endl
+             << "Please input student" << i + 1 << "'s three scores: ";
         cin >> stu[i].score[0] >> stu[i].score[1] >> stu[i].score[2];
 
         stu[i].ave = (stu[i].score[0] + stu[i].score[1] + stu[i].score[2]) / 3;
     }
 }
 
-void showstu(stuinfo stu[], int n) {
+void showstu(stuinfo stu[], int n)
+{
     for (int i = 0; i < n; i++)
     {
         cout << "Student " << stu[i].name << "'s scores are " << stu[i].score[0] << ' ' << stu[i].score[1] << ' ' << stu[i].score[2] << " with average of " << stu[i].ave << '.' << endl;
     }
 }
 
-void sortstu(stuinfo stu[], int n) {
+void sortstu(stuinfo stu[], int n)
+{
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i; j < n - 1; j++)
+        for (int j = n - 2; j >= i; j--)
         {
-            if (stu[j].ave < stu[j + 1].ave) {
+            if (stu[j].ave < stu[j + 1].ave)
+            {
                 stuinfo temp = stu[j];
                 stu[j] = stu[j + 1];
-                stu[j + 1] = temp; 
+                stu[j + 1] = temp;
             }
         }
     }
 }
 
-bool findstu(stuinfo stu[], int n, char ch[]) {
+bool findstu(stuinfo stu[], int n, char ch[])
+{
     for (int i = 0; i < n; i++)
     {
-        if (strcmp(stu[i].name, ch) == 0) {
+        if (strcmp(stu[i].name, ch) == 0)
+        {
             return true;
         }
     }
