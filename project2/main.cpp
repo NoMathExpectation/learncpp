3f9de6abd063cef6a4c2d13c92aa593f3260078f
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-10-16 14:44:00 +0800

    minor change

--- project2/main.cpp
+++ project2/main.cpp
@@ -2,31 +2,29 @@
 #include "bignum.cpp"
 #include "calculator.cpp"
 
-using namespace std;
-
 int main()
 {
     Calculator calculator;
-    string expr;
+    std::string expr;
 
-    cout << "Welcome to use this calculator!" << endl;
-    cout << "Type \"exit\" or \"exit()\" to exit the program." << endl;
+    std::cout << "Welcome to use this calculator!" << std::endl;
+    std::cout << "Type \"exit\" or \"exit()\" to exit the program." << std::endl;
 
     while (true)
     {
         try
         {
-            getline(cin, expr);
+            std::getline(std::cin, expr);
             if (expr == "exit" || expr == "exit()")
             {
                 break;
             }
 
-            cout << calculator.eval(expr) << endl;
+            std::cout << calculator.eval(expr) << std::endl;
         }
-        catch (invalid_argument &error)
+        catch (std::invalid_argument &error)
         {
-            cout << error.what() << endl;
+            std::cout << error.what() << std::endl;
         }
     }
 }
