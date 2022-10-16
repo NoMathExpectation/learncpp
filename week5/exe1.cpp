42fb618f234a885afec742127fa4f5e7e8ef446a
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-10-16 12:42:04 +0800

    week5

---
+++ week5/exe1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *p = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        p[i] = i;
+    }
+
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << p[i] << endl;
+    }
+
+    delete p;
+    return 0;
+}
