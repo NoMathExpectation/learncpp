106189317c9b9216abafdc91b62cc064dd23df0c
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2022-11-25 17:39:45 +0800

    week11 lab

---
+++ week11/lab/matrix.cpp
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <memory>
+
+class Matrix
+{
+protected:
+    size_t row;
+    size_t col;
+    std::shared_ptr<float[]> data;
+
+public:
+    Matrix(size_t row, size_t col) noexcept
+    {
+        this->row = row;
+        this->col = col;
+        data = std::make_shared<float[]>(new float[row * col]);
+    }
+
+    Matrix(const Matrix &another) noexcept
+    {
+        row = another.row;
+        col = another.col;
+        data = another.data;
+    }
+
+    Matrix(Matrix &&another) noexcept : Matrix(another)
+    {
+    }
+
+    Matrix &operator=(const Matrix &another) noexcept
+    {
+        this->row = another.row;
+        this->col = another.col;
+        this->data = another.data;
+        return *this;
+    }
+
+    Matrix &operator=(const Matrix &&another) noexcept
+    {
+        *this = another;
+        return *this;
+    }
+
+    Matrix operator+(const Matrix &another) const noexcept
+    {
+        Matrix result(row, col);
+
+        for (size_t i = 0; i < row * col; i++)
+        {
+            result.data[i] = data[i] + another.data[i];
+        }
+
+        return result;
+    }
+
+    Matrix operator+(const Matrix &&another) const noexcept {
+        return *this + another;
+    }
+
+    Matrix operator*(const float val) const noexcept
+    {
+        Matrix result(row, col);
+
+        for (size_t i = 0; i < row * col; i++)
+        {
+            result.data[i] = data[i] * val;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Matrix a(3, 4), b(3, 4);
+    Matrix c = a + b;
+    Matrix d = a * 2.0f;
+}
